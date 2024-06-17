e725e1a0644cb72e98dd33e523d948847430a01c
byungchan0725 <byungchan0725@gmail.com>
2024-06-18 08:22:30 +0900

    CORS middleware 추가

--- main.py
+++ main.py
@@ -2,8 +2,18 @@
 
 from fastapi import FastAPI, status
 from pydantic import BaseModel
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"], 
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 
 class Item(BaseModel):
