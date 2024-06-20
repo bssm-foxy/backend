94d8988b2316dacdeedb676716ae6f294ad23e4d
byungchan0725 <byungchan0725@gmail.com>
2024-06-20 17:28:05 +0900

    BaseModel 관리

---
+++ app/api/models.py
@@ -0,0 +1,12 @@
+from pydantic import BaseModel
+
+
+# 출발지와 도착지 이름 
+class PointName(BaseModel):
+    start_point: str 
+    finish_point: str 
+
+# 로봇 현재 위치 
+class RobotPosition(BaseModel):
+    x: float 
+    y: float 
