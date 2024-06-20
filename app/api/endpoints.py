70dcafb484b8a4559f24becd2d2f8c2e1a32f967
byungchan0725 <byungchan0725@gmail.com>
2024-06-20 22:55:27 +0900

    모든 기능 구현 완료

--- app/api/endpoints.py
+++ app/api/endpoints.py
@@ -1,13 +1,11 @@
-import yaml
-import asyncio
-
-from pydantic import BaseModel
-from fastapi import APIRouter, status, HTTPException
+from typing import Optional
+from fastapi import APIRouter, status, HTTPException, Body
 
 from api.models import PointName, RobotPosition
-from database.db_querys import SetPointQueryAsync, GetStartPoint, Arrived, WaitUntilArrived, \
-                                ReturnLatestStartPosition
+from database.db_querys import SetPoint, GetStartPoint, GetFinishPoint, StartArrived, FinishArrived, \
+                                WaitUntilArrived, WaitUntilArrived_1
 
+TIME_OUT = 120 # 대기시간 2분으로 설정
 
 router = APIRouter()
 
@@ -17,19 +15,19 @@ def home():
     return "Hello"
 
 
-## 로봇 출발 후 대기 시간 페이지 로딩 대기 
-async def post_point(point: PointName):
-    TIME_OUT = 120 # 대기시간 2분으로 설정
-
+# 로봇 출발 후 대기 시간 페이지 로딩 대기 
+# 목적지를 출발지로 설정 
+async def move_to_start_point(point: PointName):
     try:
-        await SetPointQueryAsync(point.start_point, point.finish_point)
-        positions = GetStartPoint()
+        await SetPoint(point.start_point, point.finish_point)  # 데이터베이스에 출발지와 도착지 설정 
 
-        point_name = positions["point_name"]
-        point_x = positions["point_x"]
-        point_y = positions["point_y"]
+        positions = GetStartPoint()  # 출발지 좌표를 가져옴 
+
+        point_name = positions["point_name"]  # 출발지 이름 
+        point_x = positions["point_x"]  # 출발지 X 좌표 
+        point_y = positions["point_y"]  # 출발지 Y 좌표
         
-        await post_move_to_goal(point_name, point_x, point_y)
+        await MoveToGoal(point_name, point_x, point_y)  # 출발지로 출발 
         
         if await WaitUntilArrived(timeout=TIME_OUT):
             return {
@@ -43,42 +41,99 @@ async def post_point(point: PointName):
         print(e)
         raise HTTPException(status_code=500, detail=str(e))
     
+async def move_to_finish_point():
+    try:
+        positions = GetFinishPoint()  # 출발지 좌표를 가져옴 
+
+        point_name = positions["point_name"]  # 출발지 이름 
+        point_x = positions["point_x"]  # 출발지 X 좌표 
+        point_y = positions["point_y"]  # 출발지 Y 좌표
+        
+        await MoveToGoal(point_name, point_x, point_y)  # 출발지로 출발 
+        
+        if await WaitUntilArrived_1(timeout=TIME_OUT):
+            return {
+                "Success_or_not": True
+                }
+        else:
+            # 타임아웃 처리
+            raise HTTPException(status_code=500, detail="대기시간이 너무 길어 Timeout 처리하였습니다.")
+
+    except Exception as e:
+        print(e)
+        raise HTTPException(status_code=500, detail=str(e))
+    
+
 @router.post("/point", status_code=status.HTTP_201_CREATED)
-async def post_point_handler(point: PointName):
-    return await post_point(point)
+async def point(point: PointName):
+    return await move_to_start_point(point)  # start_or_finish 값에 따라 목적지 변경 
+
+# 로봇에서 수화물이 적재 되었다고 신호를 보내면 POST 전송 
+@router.post("/load", status_code=status.HTTP_201_CREATED)
+async def load():
+    return await move_to_finish_point()
 
 
-## 로봇 출발 명령
+# 로봇에서 수화물이 양하 되었다고 신호를 보내면 POST 전송 
+@router.post("/unload", status_code=status.HTTP_201_CREATED)
+def unoad():
+    return {
+        "Success_or_not": True
+    }
+
+
+
+
+
+
+
 @router.post("/robot/move_to_goal", status_code=status.HTTP_201_CREATED)
-async def post_move_to_goal(point_name: str, point_x: float, point_y: float):
+async def MoveToGoal(point_name: str, point_x: float, point_y: float):
     print(f"{point_name} 으로 이동합니다. 해당좌표: ({point_x}, {point_y})")
 
     return {
         "Success_or_not": True
     }
 
-@router.get("/robot/move_to_goal", status_code=status.HTTP_200_OK)
-async def get_move_to_goal():
-    # 데이터베이스 조회 코드로 수정 필요 
-    start_positions = ReturnLatestStartPosition()
-    
-    if start_positions:
+@router.get("/robot/get_start_point",status_code=status.HTTP_200_OK) 
+async def GetStartPosition():
+    positions = GetStartPoint()
+
+    if positions:
         return {
-            "point_name": start_positions["point_name"],
-            "position_x": start_positions["position_x"],
-            "position_y": start_positions["position_y"]
+            "point_name": positions["point_name"],
+            "position_x": positions["point_x"],
+            "position_y": positions["point_y"]
         }
 
+@router.get("/robot/get_finish_point",status_code=status.HTTP_200_OK) 
+async def GetFinishPosition():
+    positions = GetFinishPoint()
+
+    if positions:
+        return {
+            "point_name": positions["point_name"],
+            "position_x": positions["point_x"],
+            "position_y": positions["point_y"]
+        }
+
+
 ## 도착시 데이터베이스 is_arrived 값 True로 변경 
-@router.post("/robot/arrived", status_code=status.HTTP_201_CREATED)
-async def RobotArrived():
-    Arrived() # 도착하면 도착 값을 True로 변경
+@router.post("/robot/start_point_arrived", status_code=status.HTTP_201_CREATED)
+async def StartPointArrived():
+    StartArrived() # 도착하면 도착 값을 True로 변경
 
     return {
         "Success_or_not": True
     }
 
+@router.post("/robot/finish_point_arrived", status_code=status.HTTP_201_CREATED)
+async def FinishPointArrived():
+    FinishArrived() # 도착하면 도착 값을 True로 변경
 
+    return {
+        "Success_or_not": True
+    }
 
 
 ## 현재 로봇 위치 파악 코드 
@@ -109,18 +164,5 @@ def GetPosition():
         "Position": TempPosition
     }
 
-# 로봇에서 수화물이 적재 되었다고 신호를 보내면 POST 전송 
-@router.post("/load", status_code=status.HTTP_201_CREATED)
-def Load():
-    # 이 사이에 도착했다는 결과가 와야함.
 
-    return {
-        "Success_or_not": True
-    }
-
-# 로봇에서 수화물이 양하 되었다고 신호를 보내면 POST 전송 
-@router.post("/unload", status_code=status.HTTP_201_CREATED)
-def Load():
-    return {
-        "Success_or_not": True
-    }
+## 
--- app/database/db_querys.py
+++ app/database/db_querys.py
@@ -5,39 +5,42 @@ import aiomysql
 from database.config.db_config import DB_CONFIG
 
         
-async def SetPointQueryAsync(start_point, finish_point):
+async def SetPoint(start_point, finish_point):
     conn = await aiomysql.connect(**DB_CONFIG)
 
     try:
         async with conn.cursor() as cursor:
-            await cursor.execute("SELECT Number FROM SetPoint WHERE TimeLog = (SELECT MAX(TimeLog) FROM SetPoint)")
+            await cursor.execute("SELECT Number FROM setPoint Where Number = (SELECT MAX(Number) FROM SetPoint);")
             result = await cursor.fetchone()
+            print(result)
 
             if result:
                 latest_number = result[0]
             else:
                 latest_number = 0 
             
-            query = f"""INSERT INTO SetPoint (TimeLog, Number, StartPoint, FinishPoint) \
-                            VALUES (CURRENT_TIMESTAMP, {latest_number + 1}, '{start_point}', '{finish_point}');
+            query = f"""INSERT INTO SetPoint (TimeLog, Number, StartPoint, FinishPoint) 
+                        VALUES (CURRENT_TIMESTAMP, {latest_number + 1}, '{start_point}', '{finish_point}');
                             
-                        INSERT INTO StartPointArrived (TimeLog, Number, GoalPoint, IsArrived) \
-                            VALUES (CURRENT_TIMESTAMP, {latest_number + 1}, '{start_point}', {False})
+                        INSERT INTO StartPointArrived (TimeLog, Number, GoalPoint, IsArrived) 
+                        VALUES (CURRENT_TIMESTAMP, {latest_number + 1}, '{start_point}', {False});
+
+                        INSERT INTO FinishPointArrived (TimeLog, Number, GoalPoint, IsArrived) 
+                        VALUES (CURRENT_TIMESTAMP, {latest_number + 1}, '{finish_point}', {False});
                     """
-                        # 도착지 데이터베이스 insert도 추가해야함
-                        
+
             await cursor.execute(query)
             await conn.commit()
             print("데이터가 생성되었습니다.")
 
     except aiomysql.Error as e:
-        print(e)
+        print("dddddd", e)
 
     finally:
         if conn:
             conn.close()
 
-
+# 출발지 좌표 
 def GetStartPoint():
     conn = pymysql.connect(**DB_CONFIG)
 
@@ -75,49 +78,79 @@ def GetStartPoint():
         if conn:
             conn.close()
 
-
-def ReturnLatestStartPosition():
+# 도착지 좌표 
+def GetFinishPoint():
     conn = pymysql.connect(**DB_CONFIG)
 
-    try:
-        with conn.cursor() as cursor:
-            query = f"""
-                    SELECT PointName, TL.Position_X, TL.Position_Y 
-                    FROM SetPoint SP
-                    JOIN Test_Locations TL ON SP.StartPoint = TL.PointName
-                    WHERE SP.Number = (SELECT MAX(Number) FROM SetPoint);
-                    """
-            cursor.execute(query)
-            positions = cursor.fetchone()
+    try: 
+        if conn.open:
+            with conn.cursor() as cursor: 
+                query = "SELECT FinishPoint FROM SetPoint Where Number = (SELECT MAX(Number) FROM SetPoint);"
+                cursor.execute(query)
+                point_name = cursor.fetchone()[0]
+                print(point_name) # 도착지 이름 출력 
+                
+                if point_name:
+                    # 수정 필요 (Test_Location)
+                    query = f"SELECT Position_X, Position_Y FROM Test_Locations WHERE PointName = '{point_name}';"
+                    cursor.execute(query)
+                    start_point = cursor.fetchone()
             
-            if positions:
-                point_name, position_x, position_y = positions 
-
-                return {
-                    "point_name": point_name,
-                    "position_x": position_x, 
-                    "position_y": position_y
-                }
-
-    except pymysql.Error as e: 
-        print("ReturnLatestStartPosition Error")
-        print(e)
+                    if start_point:
+                        point_x, point_y = start_point[0], start_point[1]
+                        print(f"{point_name} 좌표 ({point_x}, {point_y})가 리턴되었습니다.")
+                        return {
+                            "point_name": point_name,
+                            "point_x": point_x,
+                            "point_y": point_y
+                        }
+                    else:
+                        print(f"{point_name}에 해당하는 좌표가 데이터베이스에 없습니다.")
+                else:
+                    print("최근 SetPoint 데이터가 없습니다.")
     
+    except pymysql.Error as e:
+        print(e)
+
     finally:
         if conn:
             conn.close()
 
 
 ## 목적지로 도착시 Ture 반환 코드 --- 
-def Arrived():
+def StartArrived():
     conn = pymysql.connect(**DB_CONFIG)
 
     try:
         with conn.cursor() as cursor:
-            # 도착시 is_arrived 값을 True로 변경 
+            # 출발지 도착시 is_arrived 값을 True로 변경 
             query = """
                     UPDATE StartPointArrived AS T1
                     JOIN (SELECT MAX(Number) AS MaxNumber FROM StartPointArrived) AS T2
+                    ON T1.Number = T2.MaxNumber
+                    SET T1.IsArrived = True;
+                    """
+            cursor.execute(query)
+            conn.commit()
+
+    except pymysql.Error as e:
+        print(f"데이터를 수정하는 과정에서 에러가 발생하였습니다: {e}")
+
+    finally:
+        if conn:
+            conn.close()
+    
+       
+## 목적지로 도착시 Ture 반환 코드 --- 
+def FinishArrived():
+    conn = pymysql.connect(**DB_CONFIG)
+
+    try:
+        with conn.cursor() as cursor:
+            # 도착지 도착시 is_arrived 값을 True로 변경 
+            query = """
+                    UPDATE FinishPointArrived AS T1
+                    JOIN (SELECT MAX(Number) AS MaxNumber FROM FinishPointArrived) AS T2
                     ON T1.Number = T2.MaxNumber
                     SET T1.IsArrived = True;
                     """
@@ -167,6 +200,43 @@ async def WaitUntilArrived(timeout: int) -> bool:  # 도착할 때까지 계속 
         else:
             await asyncio.sleep(1)  # 1초 대기 후 다시 체크
 
+async def WaitUntilArrived_1(timeout: int) -> bool:  # 도착할 때까지 계속 확인 
+    start_time = asyncio.get_event_loop().time()
+
+    while True:
+        is_arrived = CheckArrivedValue_1()
+        if is_arrived:
+            return True
+        elif asyncio.get_event_loop().time() - start_time >= timeout:
+            return False
+        else:
+            await asyncio.sleep(1)  # 1초 대기 후 다시 체크
+
+
+def CheckArrivedValue_1():  # 도착하였는지 확인하는 코드 
+    conn = pymysql.connect(**DB_CONFIG)
+
+    try:
+        with conn.cursor() as cursor:
+            query = """
+                    SELECT IsArrived FROM FinishPointArrived \
+                        WHERE Number = (SELECT MAX(Number) FROM FinishPointArrived);
+                    """
+            cursor.execute(query)
+            result = cursor.fetchone()
+
+            if result:
+                return result[0]
+            else:
+                print("데이터가 없음")
+
+    except pymysql.Error as e:
+        print(e)
+
+    finally:
+        if conn:
+            conn.close()
+
 
 if __name__ == "__main__":
-    ReturnLatestStartPosition()
+    GetFinishPoint()
