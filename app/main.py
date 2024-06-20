6e72db18fb90f0efc8b2d0e0851a0a06319abf8b
byungchan0725 <byungchan0725@gmail.com>
2024-06-20 22:59:00 +0900

    리액트 접속 CORS 추가

--- app/main.py
+++ app/main.py
@@ -1,13 +1,21 @@
 import uvicorn
 
 from fastapi import FastAPI
-from endpoints import router
+from api.endpoints import router
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
-# 엔드포인트 라우터 추가
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"], 
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(router)
 
-# FastAPI 서버 실행 (uvicorn을 사용하는 경우)
+
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
